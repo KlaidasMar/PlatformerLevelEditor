079c44d082acfea8105d8e3c889cce8fef2e282a
Klaidas Markisius <markisius.klaidas@gmail.com>
2023-03-08 15:42:50 +0200

    -

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import button
+import csv
 
 pygame.init()
 
@@ -18,6 +19,7 @@ ROWS = 16
 MAX_COLS = 150
 TILE_SIZE = SCREEN_HEIGHT // ROWS
 TILE_TYPES = 21
+level = 0
 current_tile = 0
 scroll_left = False
 scroll_right = False
@@ -32,13 +34,18 @@ sky_img = pygame.image.load('assets/Background/sky_cloud.png').convert_alpha()
 
 img_list = []
 for x in range(TILE_TYPES):
-    img = pygame.image.load(f"assets/tile/{x}.png")
+    img = pygame.image.load(f"assets/tile/{x}.png").convert_alpha()
     img = pygame.transform.scale(img, (TILE_SIZE, TILE_SIZE))
     img_list.append(img)
+
+save_img = pygame.image.load('assets/save_btn.png').convert_alpha()
+load_img = pygame.image.load('assets/load_btn.png').convert_alpha()
 
 GREEN = (65, 106, 41)
 WHITE = (255, 255, 255)
 RED = (200, 25, 25)
+
+font = pygame.font.SysFont('Futura', 30)
 
 world_data = []
 for row in range(ROWS):
@@ -48,6 +55,10 @@ for row in range(ROWS):
 for tile in range(0, MAX_COLS):
     world_data[ROWS - 1][tile] = 0
 
+
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
 
 def draw_bg():
     screen.fill(GREEN)
@@ -73,6 +84,8 @@ def draw_world():
             if tile >= 0:
                 screen.blit(img_list[tile], (x * TILE_SIZE - scroll, y * TILE_SIZE))
 
+save_button = button.Button(SCREEN_WIDTH // 2, SCREEN_HEIGHT + LOWER_MARGIN - 50, save_img, 1)
+load_button = button.Button(SCREEN_WIDTH // 2 + 200, SCREEN_HEIGHT + LOWER_MARGIN - 50, load_img, 1)
 
 button_list = []
 button_col = 0
@@ -95,6 +108,24 @@ while run:
     draw_grid()
     draw_world()
 
+    draw_text(f'Level: {level}', font, WHITE, 10, SCREEN_HEIGHT + LOWER_MARGIN - 90)
+    draw_text(f'Press UP or DOWN to change level', font, WHITE, 10, SCREEN_HEIGHT + LOWER_MARGIN - 60)
+
+    if save_button.draw(screen):
+        with open(f'level{level}_data.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter = ',')
+            for row in world_data:
+                writer.writerow(row)
+
+    if load_button.draw(screen):
+        scroll = 0
+        with open(f'level{level}_data.csv',  newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter = ',')
+            for x, row in enumerate(reader):
+                for y, tile in enumerate(row):
+                    world_data[x][y] = int(tile)
+
+
     pygame.draw.rect(screen, GREEN, (SCREEN_WIDTH, 0, SIDE_MARGIN, SCREEN_HEIGHT))
 
     button_count = 0
@@ -106,7 +137,7 @@ while run:
 
     if scroll_left == True and scroll > 0:
         scroll -= 5 * scroll_speed
-    if scroll_right == True:
+    if scroll_right == True and scroll < (MAX_COLS * TILE_SIZE) - SCREEN_WIDTH:
         scroll += 5 * scroll_speed
 
     pos = pygame.mouse.get_pos()
@@ -117,12 +148,18 @@ while run:
         if pygame.mouse.get_pressed()[0] == 1:
             if world_data[y][x] != current_tile:
                 world_data[y][x] = current_tile
+        if pygame.mouse.get_pressed()[2] == 1:
+            world_data[y][x] = -1
 
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                level += 1
+            if event.key == pygame.K_DOWN and level > 0:
+                level -= 1
             if event.key == pygame.K_LEFT:
                 scroll_left = True
             if event.key == pygame.K_RIGHT:
